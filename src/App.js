4ca0bcd46e7ff1dd13f2a390bdb3f26000ca4351
AndrewPechnikov <73440973+AndrewPechnikov@users.noreply.github.com>
2023-09-01 16:24:38 +0300

    feature: cart-markdown

--- src/App.js
+++ src/App.js
@@ -14,6 +14,73 @@ import { ReactComponent as Check } from './img/check.svg'
 function App() {
   return <div className="wrapper clear">
 
+    <div className="overlay">
+      <div className="drawer d-flex flex-column">
+        <h2 className='mb-30' >Корзина</h2>
+
+        <div className="items">
+          <div className="cartItem d-flex align-center justify-between">
+            <img width={70} height={58.9} src={sneakers} alt="Кросівки" />
+            <div className='cartItem__text'>
+              <p className='mb-5'>Чоловічі кроссівки Nike Kyrie 7</p>
+              <b>3 999 грн.</b>
+
+            </div>
+            <button className='button button__remove'>
+              <Plus className='remove' width={11} height={11} />
+            </button>
+          </div>
+
+          <div className="cartItem d-flex align-center justify-between">
+            <img width={70} height={58.9} src={sneakers} alt="Кросівки" />
+            <div className='cartItem__text'>
+              <p className='mb-5'>Чоловічі кроссівки Nike Kyrie 7</p>
+              <b>3 999 грн.</b>
+
+            </div>
+            <button className='button button__remove'>
+              <Plus className='remove' width={11} height={11} />
+            </button>
+          </div>
+
+          <div className="cartItem d-flex align-center justify-between">
+            <img width={70} height={58.9} src={sneakers} alt="Кросівки" />
+            <div className='cartItem__text'>
+              <p className='mb-5'>Чоловічі кроссівки Nike Kyrie 7</p>
+              <b>3 999 грн.</b>
+
+            </div>
+            <button className='button button__remove'>
+              <Plus className='remove' width={11} height={11} />
+            </button>
+
+          </div>
+
+
+
+
+
+
+        </div>
+
+        <div className='cartTotalBlock'>
+          <ul >
+            <li className=''>
+              <span>Всього</span>
+              <div></div>
+              <b>5 320 грн.</b>
+            </li>
+            <li className=''>
+              <span>Доставка 50 грн.</span>
+              <div></div>
+              <b>4 270 грн.</b>
+            </li>
+          </ul>
+          <button>Оформити замовлення</button>
+        </div>
+      </div>
+
+    </div>
     <header className='d-flex justify-between align-center p-40'>
       <div className="d-flex align-center">
         <img className='mr-10' width={40} height={40} src={logo} alt="" />
