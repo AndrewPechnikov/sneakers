9db2e5b6fe6bd1cfb44e2fa63e4c125db7b09fc9
AndrewPechnikov <73440973+AndrewPechnikov@users.noreply.github.com>
2023-09-11 17:15:03 +0300

    feature: add get from mockapi

--- src/App.js
+++ src/App.js
@@ -1,78 +1,62 @@
-
-
-import { ReactComponent as Search } from './img/search.svg'
-import Card from './components/Card'
-import Header from './components/Header/Header'
-import Drawer from './components/Drawer/Drawer'
-import sneakers1 from './img/Sneakers/1.jpg'
-import sneakers2 from './img/Sneakers/2.jpg'
-import sneakers3 from './img/Sneakers/3.jpg'
-import sneakers4 from './img/Sneakers/4.jpg'
-import { useEffect, useState } from 'react'
-
-
-
-
-
-
-
-
-
-
+import { ReactComponent as Search } from './img/search.svg';
+import Card from './components/Card';
+import Header from './components/Header/Header';
+import Drawer from './components/Drawer/Drawer';
+import { useEffect, useState } from 'react';
 
 function App() {
-
   useEffect(() => {
-    fetch("https://64f9bcfb4098a7f2fc14e8a4.mockapi.io/itemas")
-      .then(res => {
+    fetch('https://64f9bcfb4098a7f2fc14e8a4.mockapi.io/itemas')
+      .then((res) => {
         return res.json();
       })
       .then((json) => {
-        setItems(json)
-      })
+        setItems(json);
+      });
+  }, []);
 
-  }, [])
+  const [items, setItems] = useState([]);
+  const [cartOpened, setCartOpened] = useState(false);
 
+  return (
+    <div className="wrapper clear">
+      {cartOpened && (
+        <Drawer
+          onClickClose={() => {
+            setCartOpened(false);
+          }}
+        />
+      )}
 
+      <Header
+        onClickCart={() => {
+          setCartOpened(true);
+        }}
+      />
 
-  const [items, setItems] = useState([])
-
-  const [cartOpened, setCartOpened] = useState(false)
-  return <div className="wrapper clear">
-
-    {cartOpened && <Drawer onClickClose={() => { setCartOpened(false) }} />}
-
-    <Header onClickCart={() => { setCartOpened(true) }} />
-
-    <div className="content p-40">
-
-      <div className='d-flex align-center justify-between mb-40'>
-        <h1 >Всі кросівки</h1>
-        <div className='search-blok d-flex align-center '>
-          <Search alt="search" />
-          <input placeholder='Пошук...' />
+      <div className="content p-40">
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>Всі кросівки</h1>
+          <div className="search-blok d-flex align-center ">
+            <Search alt="search" />
+            <input placeholder="Пошук..." />
+          </div>
         </div>
-
-
+        <div className="card-list">
+          {items.map((obj, index) => {
+            return (
+              <Card
+                key={index}
+                title={obj.name}
+                price={obj.price}
+                img={obj.url}
+              />
+            );
+          })}
+        </div>
       </div>
-
-
-      <div className="card-list">
-        {
-          items.map((obj, index) => {
-            return <Card key={index} title={obj.name} price={obj.price} img={obj.sneakers} />
-          })
-        }
-
-      </div>
-
-
-
-
-
     </div>
-
-  </div>
+  );
 }
 
 export default App;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,65 +1,67 @@
-import { ReactComponent as Heart } from './../../img/heart.svg'
-import { ReactComponent as Check } from './../../img/check.svg'
-import { ReactComponent as HeartActive } from './../../img/heart-active.svg'
-import { useEffect, useState } from 'react'
-import { ReactComponent as Plus } from './../../img/plus.svg'
-import styles from './Card.module.scss'
+import { ReactComponent as Heart } from './../../img/heart.svg';
+import { ReactComponent as Check } from './../../img/check.svg';
+import { ReactComponent as HeartActive } from './../../img/heart-active.svg';
+import { useEffect, useState } from 'react';
+import { ReactComponent as Plus } from './../../img/plus.svg';
+import styles from './Card.module.scss';
 console.log(styles);
 
-
-
-
-
 const Card = ({ title, price, img }) => {
-    const [isAddedProduct, setAddedProductState] = useState(false)
-    const [isAddFavorite, setAddedFavoriteState] = useState(false)
+    const [isAddedProduct, setAddedProductState] = useState(false);
+    const [isAddFavorite, setAddedFavoriteState] = useState(false);
 
     const onClickAddProduct = () => {
-        setAddedProductState(!isAddedProduct)
-    }
+        setAddedProductState(!isAddedProduct);
+    };
 
     const onClickAddFavoriteProduct = () => {
-        setAddedFavoriteState(!isAddFavorite)
-    }
+        setAddedFavoriteState(!isAddFavorite);
+    };
 
-    useEffect(() => {
-
-    }, [isAddedProduct, isAddFavorite])
-
+    useEffect(() => { }, [isAddedProduct, isAddFavorite]);
 
     return (
         <div className={styles.card}>
-            {(isAddFavorite) ?
-                <button onClick={onClickAddFavoriteProduct} className='button button__heart button__heart--active d-flex justify-center align-center'>
+            {isAddFavorite ? (
+                <button
+                    onClick={onClickAddFavoriteProduct}
+                    className="button button__heart button__heart--active d-flex justify-center align-center"
+                >
                     <HeartActive width={14} height={13} />
-                </button> :
-                <button onClick={onClickAddFavoriteProduct} className='button button__heart d-flex justify-center align-center'>
+                </button>
+            ) : (
+                <button
+                    onClick={onClickAddFavoriteProduct}
+                    className="button button__heart d-flex justify-center align-center"
+                >
                     <Heart width={14} height={13} />
                 </button>
-            }
+            )}
 
             <img width={153} src={img} alt="" />
             <h5>{title}</h5>
 
-            <div className='d-flex justify-between align-center'>
-
-                <div className='d-flex flex-column '>
+            <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column ">
                     <span>Ціна</span>
                     <b>{price}</b>
                 </div>
 
-                {(isAddedProduct) ?
-                    <button className='button button__plus--checked' onClick={onClickAddProduct}>
+                {isAddedProduct ? (
+                    <button
+                        className="button button__plus--checked"
+                        onClick={onClickAddProduct}
+                    >
                         <Check width={11} height={11} />
-                    </button> :
-                    <button className='button button__plus' onClick={onClickAddProduct}>
+                    </button>
+                ) : (
+                    <button className="button button__plus" onClick={onClickAddProduct}>
                         <Plus width={11} height={11} />
                     </button>
-                }
-
-            </div >
-        </div >
-    )
-}
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default Card;
